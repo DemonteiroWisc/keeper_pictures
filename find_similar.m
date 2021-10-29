3253a614f2b286731d452f1a7b7c5cb1b3fa8c74
Daniel de Monteiro <demonteiro@wisc.edu>
2021-10-29 14:47:33 -0500

    test_images collage

--- find_similar.m
+++ find_similar.m
@@ -1,6 +1,20 @@
 function [outputArg1,outputArg2] = find_similar(inputArg1,inputArg2)
 %FIND_SIMILAR Summary of this function goes here
 %   Detailed explanation goes here
+
+dataDirectory = fullfile('data');
+test_images = imageDatastore(dataDirectory);
+
+thumbnailGallery = [];
+for i = 1:length(test_images.Files)
+    I = readimage(test_images,i);
+    thumbnail = imresize(I,[450 600]);
+    thumbnailGallery = cat(4,thumbnailGallery,thumbnail);
+end
+
+figure
+montage(thumbnailGallery);
+
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 end
